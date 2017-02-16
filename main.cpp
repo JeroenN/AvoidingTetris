fe29b16e7c9609ce6c6d5a2c375bf415e09600d4
JeroenN <JeroenN@users.noreply.github.com>
2017-02-16 23:31:24 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,13 +13,11 @@ class standingBlock
 
 public:
     standingBlock(const float height, const float width, const float posX, const float posY)
-       // :standingBlockHeight(height),  standingBlockWidth(width), standingBlock_x(posX), standingBlock_y(posY)
         :block()
     {
         block.setSize(sf::Vector2f(height, width));
         block.setPosition(sf::Vector2f(posX, posY));
         block.setFillColor(sf::Color(255, 127, 63));
-        //block.setOrigin(sf::Vector2f(0,0));
     }
 
     float getHeight()const {
@@ -60,16 +58,33 @@ std::vector<standingBlock> create_standing_blocks() noexcept
     return v;
 }
 
+std::vector<standingBlock> create_L_block() noexcept
+{
+    std::vector<standingBlock> v;
+    v.push_back(standingBlock(30,30,100,0));
+    v.push_back(standingBlock(30,30,100,30));
+    v.push_back(standingBlock(30,30,100,60));
+    v.push_back(standingBlock(30,30,130,60));
+    return v;
+}
+
 ///Does the player have a collission with a standing block at his/her right side?
 bool player_collides_right(
     const float posX,
     const float posY,
     const float playerSizeX,
     const float playerSizeY,
-    const standingBlock& standingBlock,
-    const bool movingRight
+    const standingBlock& standingBlock
 )
 {
+    bool movingRight=false;
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)  || sf::Keyboard::isKeyPressed(sf::Keyboard::D)){
+            movingRight =true;
+    }
+    else{
+       movingRight =false;
+    }
+
     return posY +playerSizeY > standingBlock.getPosY()
       && posY < standingBlock.getPosY()+standingBlock.getHeight()
       && posX +playerSizeX <standingBlock.getPosX() + 20
@@ -77,9 +92,73 @@ bool player_collides_right(
       && movingRight
     ;
 }
+
+///Does the player have collission with a standing block at his/her left side?
+bool player_collides_left(
+    const float posX,
+    const float posY,
+    const float playerSizeY,
+    const standingBlock& standingBlock
+    //const bool movingLeft
+)
+{
+    bool movingLeft=false;
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) || sf::Keyboard::isKeyPressed(sf::Keyboard::A)){
+            movingLeft =true;
+    }
+    else{
+       movingLeft =false;
+    }
+    return posY +playerSizeY > standingBlock.getPosY()
+      && posY < standingBlock.getPosY()+standingBlock.getHeight()
+      && posX > standingBlock.getPosX() + standingBlock.getWidth()-20
+      && posX< standingBlock.getPosX() +standingBlock.getWidth()
+      && movingLeft
+     ;
+}
+
+bool player_collides_head(
+        const float posX,
+        const float posY,
+        const float playerSizeX,
+        const standingBlock& standingBlock
+
+        )
+{
+   return posY > standingBlock.getPosY()+20
+       && posY < standingBlock.getPosY()+standingBlock.getHeight()
+       && posX + playerSizeX> standingBlock.getPosX()
+       && posX < standingBlock.getPosX()+standingBlock.getWidth()
+      ;
+}
+
+bool player_collides_bottem(
+        const float posX,
+        const float posY,
+        const float playerSizeX,
+        const float playerSizeY,
+        const standingBlock& standingBlock
+      )
+{
+      if((posY+playerSizeY >standingBlock.getPosY()
+      && posY+playerSizeY < standingBlock.getPosY()+20)
+      || posY==standingBlock.getPosY()-playerSizeY)
+      {
+
+      if(
+       posX + playerSizeX> standingBlock.getPosX()
+       && posX < standingBlock.getPosX()+standingBlock.getWidth())
+        {
+          return true;
+        }
+     }
+
+      return false;
+}
+
 float moving_player_horizontally(float posX)
 {
-    float speed =10;
+    float speed =8;
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)
         || sf::Keyboard::isKeyPressed(sf::Keyboard::D))
     {
@@ -95,7 +174,6 @@ float moving_player_horizontally(float posX)
 
 float jump_player_vertically()
 {
-    //float jumpingForce=0;
     bool jumpReady=true;
     bool jumping=false;
 
@@ -114,20 +192,40 @@ float jump_player_vertically()
     return jumping;
 }
 
+float jumping_gravity(float jumpingForce, bool isGrounded)
+{
+    float gravity=0.9;
+    if(isGrounded ==false)
+    {
+        if(jumpingForce<=14)
+        jumpingForce+=gravity;
+    }
+    return jumpingForce;
+}
+float player_screenborder_collision(float posX, float windowSizeX, float playerSizeX)
+{
+    if(posX<0)
+    {
+        posX=0;
+    }
+    if(posX+playerSizeX>windowSizeX)
+    {
+        posX= windowSizeX-playerSizeX;
+    }
+    return posX;
+}
+
 int main()
 {
-    std::vector<standingBlock> standingBlocks = create_standing_blocks();
+   // std::vector<standingBlock> standingBlocks = create_standing_blocks();
+    std::vector<standingBlock> standingBlocks = create_L_block();
     int posX=300;
     int windowSizeX=700;
     int windowSizeY = 900;
     int playerSizeX=25;
     int playerSizeY=40;
     float jumpingForce = 0;
-    float gravity = 1;
     bool isGrounded =true;
-    bool movingRight =false;
-    bool movingLeft = false;
-   // bool jumpReady = true;
     std::vector<sf::RectangleShape> blocks;
     int posY = windowSizeY-playerSizeY;
     sf::RenderWindow window(
@@ -152,31 +250,18 @@ int main()
                 break;
             }
         }
-
-    standingBlocks[0].moveDown(standingBlocks[0].getPosX(),standingBlocks[0].getPosY(),2);
+    for(int i=0; i<4; ++i)
+    {
+    standingBlocks[i].moveDown(standingBlocks[i].getPosX(),standingBlocks[i].getPosY(),2);
+    }
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
     {
         window.close();
         return 0;
     }
 
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) || sf::Keyboard::isKeyPressed(sf::Keyboard::A))
-    {
-            movingLeft =true;
-    }
-    else
-    {
-       movingLeft =false;
-    }
 
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)  || sf::Keyboard::isKeyPressed(sf::Keyboard::D))
-    {
-            movingRight =true;
-    }
-    else
-    {
-       movingRight =false;
-    }
+
 
     posX = moving_player_horizontally(posX);
     if(posY+playerSizeY==windowSizeY || posY+playerSizeY>windowSizeY)
@@ -188,69 +273,62 @@ int main()
     {
        isGrounded = false;
     }
-
     //Colision detection
-
     for(int i=0; i<static_cast<int>(standingBlocks.size()); ++i)
     {
-        //Standing on a block
-        if((posY+playerSizeY >standingBlocks[i].getPosY() && posY+playerSizeY < standingBlocks[i].getPosY()+20) || posY==standingBlocks[i].getPosY()-playerSizeY)
-        {
-            if(posX + playerSizeX> standingBlocks[i].getPosX() && posX < standingBlocks[i].getPosX()+standingBlocks[i].getWidth())
-            {
-                if(jumpingForce>0)
-                {
-                    isGrounded=true;
-                    //Needs to be a true or false statement: if( blockMovingDown ==true)
-                    if(i!=0)
-                    {
-                        posY = standingBlocks[i].getPosY()-playerSizeY;
-                    }
-                    if(i==0)
-                    {
-                        posY = standingBlocks[i].getPosY()-playerSizeY;
-                        posY+=2;
-                    }
-                    std::cout<<i<<'\n';
 
-                }
+        if(player_collides_bottem(
+            posX,
+            posY,
+            playerSizeX,
+            playerSizeY,
+            standingBlocks[i]) == true)
+        {
+            if(jumpingForce>0)
+            {
+                isGrounded=true;
+                //Needs to be a true or false statement: if( blockMovingDown ==true)
+                posY = standingBlocks[i].getPosY()-playerSizeY;
+                std::cout<<posY<<"\n";
+               // if(i==0)
+
+                    posY+=2;
 
             }
         }
 
         //Hitting a block with your head
-        if(posY > standingBlocks[i].getPosY()+20 && posY < standingBlocks[i].getPosY()+standingBlocks[i].getHeight())
-          {
-            if(posX + playerSizeX> standingBlocks[i].getPosX() && posX < standingBlocks[i].getPosX()+standingBlocks[i].getWidth())
+        if(player_collides_head(
+            posX,
+            posY,
+            playerSizeX,
+            standingBlocks[i]) == true)
+        {
+            if(jumpingForce<0)
             {
-                if(jumpingForce<0)
-                {
-                        jumpingForce=0;
-                }
-                if(isGrounded==true)
-                {
-                    std::cout<<"You Dead";
-                }
+                    jumpingForce=0;
+            }
+            if(isGrounded==true)
+            {
+                std::cout<<"You Dead";
             }
         }
 
-        //Colliding left side player
-        if(posY +playerSizeY > standingBlocks[i].getPosY() && posY < standingBlocks[i].getPosY()+standingBlocks[i].getHeight())
-          {
-            if(posX > standingBlocks[i].getPosX() + standingBlocks[i].getWidth()-20 && posX< standingBlocks[i].getPosX() +standingBlocks[i].getWidth())
-            {
-                    if(movingLeft==true)
-                    {
-                        isGrounded=false;
-                        posX=standingBlocks[i].getPosX()+standingBlocks[i].getWidth();
-                        posY+=2;
-                        jumpingForce=0;
-                        if(posX ==standingBlocks[i].getWidth()+standingBlocks[i].getPosY())
-                        {
-                        posY+=1.5;
-                        }
-                   }
 
+        //Collding left side player
+        if (player_collides_left(
+            posX,
+            posY,
+            playerSizeY,
+            standingBlocks[i]) == true)
+        {
+            isGrounded=false;
+            posX=standingBlocks[i].getPosX()+standingBlocks[i].getWidth();
+            posY+=2;
+            jumpingForce=0;
+            if(posX ==standingBlocks[i].getWidth()+standingBlocks[i].getPosY())
+            {
+            posY+=1.5;
             }
         }
 
@@ -260,8 +338,7 @@ int main()
             posY,
             playerSizeX,
             playerSizeY,
-            standingBlocks[i],
-            movingRight) == true)
+            standingBlocks[i]) == true)
         {
             isGrounded=false;
             posX=standingBlocks[i].getPosX()-playerSizeX;
@@ -274,6 +351,7 @@ int main()
 
         }
     }
+    posX= player_screenborder_collision(posX,windowSizeX, playerSizeX);
 
     if(isGrounded==true)
     {
@@ -283,15 +361,11 @@ int main()
               isGrounded =false;
           }
      }
-
-    if(isGrounded ==false)
+    jumpingForce= jumping_gravity(jumpingForce, isGrounded);
+    if(!isGrounded)
     {
-    if(jumpingForce<=14)
-    jumpingForce+=gravity;
-    posY += jumpingForce;
+    posY+=jumpingForce;
     }
-
-
     sf::RectangleShape player(sf::Vector2f(playerSizeX, playerSizeY));
     player.setPosition(posX, posY);
     player.setOrigin(sf::Vector2f(0,0));
@@ -307,5 +381,4 @@ int main()
     }
     return 0;
 }
-
 
